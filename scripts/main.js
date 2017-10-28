b8b92845360851250cd4c7871fd6688fb07604a8
Khai Bui <khaivubui@gmail.com>
2017-10-28 14:05:02 -0700

    Write loadMessages

--- scripts/main.js
+++ scripts/main.js
@@ -66,6 +66,16 @@ FriendlyChat.prototype.initFirebase = function() {
 // Loads chat messages history and listens for upcoming ones.
 FriendlyChat.prototype.loadMessages = function() {
   this.messagesRef = this.database.ref('messages');
+  this.messagesRef.off();
+
+  const setMessage = function (data) {
+    const val = data.val();
+    this.displayMessage(
+      data.key, val.name, val.text, val.photoURL, val.imageUrl
+    );
+  }.bind(this);
+  this.messagesRef.limitToLast(12).on('child_added', setMessage);
+  this.messagesRef.limitToLast(12).on('child_changed', setMessage);
 };
 
 // Saves a new message on the Firebase DB.
